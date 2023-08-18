dbeefff3fdbac8cfd485efe2a8deb3eedbc4cf83
desAndres <locodrew@gmail.com>
2023-08-18 17:23:03 +0300

    remade comment delete get methods

--- main.py
+++ main.py
@@ -9,68 +9,110 @@ twit_id_counter = 0
 comment_id_counter = 0
 app = Flask(__name__)
 
+#Add new twit
 @app.route("/twit", methods=['POST'])
 def create_twit():
     global twit_id_counter
     twit_json = request.get_json()
+    body = twit_json.get('body')
+    if body is None:
+        return jsonify({'status': 'error', 'message': 'Comment body is required'}), 400
     twit = Twit(twit_id_counter, twit_json['body'], twit_json['author'])
     twits.append(twit)
     twit_id_counter += 1
     return jsonify({'status': 'success'})
 
+#Read all twits
 @app.route('/twit', methods=['GET'])
 def read_twits():
     serialized_twits = [{'twit_id': twit.twit_id, 'body': twit.body, 'author': twit.author} for twit in twits]
     return jsonify({'twits': serialized_twits})
 
+#Read specific twit
 @app.route('/twit/<int:twit_id>', methods=['GET'])
 def read_twit(twit_id):
     twit = next((twit for twit in twits if twit.twit_id == twit_id), None)
     if twit:
         return jsonify({'twit': {'twit_id': twit.twit_id, 'body': twit.body, 'author': twit.author}})
+    else:
+        return jsonify({'status': 'error', 'message': f'comment {twit_id} not found'})
 
+#Delete the twit
 @app.route('/twit/<int:twit_id>', methods=['DELETE'])
 def delete_twit(twit_id):
     global twits
-    twits = [twit for twit in twits if twit.twit_id != twit_id]
-    return jsonify({'status': 'success', 'message': f'Twit {twit_id} deleted'})
+    twit_to_delete = next((twit for twit in twits if twit.twit_id == twit_id), None)
+    if twit_to_delete:
+        twits = [twit for twit in twits if twit != twit_to_delete]
+        return jsonify({'status': 'success', 'message': f'twit {twit_id} deleted'})
+    else:
+        return jsonify({'status': 'error', 'message': f'comment {twit_id} not found'})
 
+#Change the twit
 @app.route('/twit/<int:twit_id>', methods=['PUT'])
 def update_twit(twit_id):
+    twit_to_update = next((twit for twit in twits if twit.twit_id == twit_id), None)
     twit_json = request.get_json()
-    for twit in twits:
-        if twit.twit_id == twit_id:
-           twit.body = twit_json['body']
-           return jsonify({'status': 'success', 'message': f'Twit {twit_id} updated'})
+    if twit_to_update:
+        for twit in twits:
+               twit.body = twit_json['body']
+               return jsonify({'status': 'success', 'message': f'Twit {twit_id} updated'})
+    else:
+        return jsonify({'status': 'error', 'message': f'Twit {twit_id} not found'})
 
-@app.route("/comment", methods=['POST'])
-def create_comment():
+#Add new comment
+@app.route('/twit/<int:twit_id>/comments', methods=['POST'])
+def create_comment_for_twit(twit_id):
     global comment_id_counter
     comment_json = request.get_json()
-    comment = Comment(comment_id_counter, comment_json['body'], comment_json['author'],comment_json['twit_id'])
+    body = comment_json.get('body')
+    author = comment_json.get('author')
+
+    if body is None:
+        return jsonify({'status': 'error', 'message': 'Comment body is required'}), 400
+
+    comment = Comment(body, twit_id, comment_id_counter, author)
     comments.append(comment)
     comment_id_counter += 1
-    return jsonify({'status': 'success'})
+    return jsonify({'status': 'success', 'message': 'Comment created'})
 
-@app.route('/comment', methods=['GET'])
-def read_comments():
-    serialized_comments = [{'comment_id': comment.comment_id, 'body': comment.body, 'author': comment.author, 'twit_id':comment.twit_id} for comment in comments]
-    return jsonify({'comments':  serialized_comments})
+#Read all comments to the twit
+@app.route('/twit/<int:twit_id>/comments', methods=['GET'])
+def read_comments(twit_id):
+    comments_for_twit = [comment for comment in comments if comment.get_twit_id() == twit_id]
+    if comments_for_twit:
+        serialized_comment = [{'comment_id': comment.comment_id,
+                               'body': comment.body,
+                               'author': comment.author,
+                               'twit_id': comment.twit_id} for comment in comments_for_twit]
+        return jsonify(serialized_comment)
+    else:
+         return jsonify({'status': 'error', 'message': f'comment {comment_id} not found'})
 
-@app.route('/comment/<int:comment_id>', methods=['DELETE'])
-def delete_comment(comment_id):
+#Read the specific comment
+@app.route('/twit/<int:twit_id>/comments/<int:comment_id>', methods=['GET'])
+def read_comment(twit_id,comment_id):
+    comment_for_twit = next((comment for comment in comments if comment.get_id() == comment_id and comment.get_twit_id() == twit_id),None)
+    if comment_for_twit:
+        serialized_comment = [{'comment_id': comment_for_twit.comment_id,
+                               'body': comment_for_twit.body,
+                               'author': comment_for_twit.author,
+                               'twit_id': comment_for_twit.twit_id}]
+        return jsonify(serialized_comment)
+    else:
+         return jsonify({'status': 'error', 'message': f'comment {comment_id} not found'})
+
+#Delete comment
+@app.route('/twit/<int:twit_id>/comments/<int:comment_id>', methods=['DELETE'])
+def delete_comment(twit_id, comment_id):
     global comments
-    comments = [comment for comment in comments if comment.comment_id != comment_id]
-    return jsonify({'status': 'success', 'message': f'comment {comment_id} deleted'})
+    comment_to_delete = next((comment for comment in comments if comment.get_twit_id() == twit_id and comment.get_id() == comment_id), None)
 
-#Чтение всех комментариев к определенному посту по twit_id
-@app.route('/comment/<int:twit_id>', methods=['GET'])
-def read_comment(twit_id):
-    comments_for_twit = [comment for comment in comments if comment.twit_id == twit_id]
-    serialized_comments = [{'comment_id': comment.comment_id, 'body': comment.body, 'author': comment.author, 'twit_id': comment.twit_id} for comment in comments_for_twit]
-    return jsonify({'comments': serialized_comments})
-
-
+    if comment_to_delete:
+        comments = [comment for comment in comments if comment != comment_to_delete]
+        return jsonify({'status': 'success', 'message': f'comment {comment_id} deleted'})
+    else:
+        return jsonify({'status': 'error', 'message': f'comment {comment_id} not found'})
 
 if __name__=='__main__':
    app.run(debug=True)
--- model/comment.py
+++ model/comment.py
@@ -1,10 +1,24 @@
-from model.twit import Twit
+# from model.twit import Twit
 from model.user import User
+#
+# class Comment:
+#
+#     def __init__(self, comment_id: int, body: str, author: User, twit_id: Twit):
+#         self.comment_id = comment_id
+#         self.body = body
+#         self.author = author
+#         self.twit_id = twit_id
+
 
 class Comment:
-
-    def __init__(self, comment_id: int, body: str, author: User, twit_id: Twit):
-        self.comment_id = comment_id
+    def __init__(self, body: str, twit_id: int, comment_id: int, author: User):
         self.body = body
-        self.author = author
         self.twit_id = twit_id
+        self.comment_id = comment_id
+        self.author = author
+
+    def get_id(self):
+        return self.comment_id
+
+    def get_twit_id(self):
+        return self.twit_id
